bbf728807d6113faf3345251c3e0dc5f7ca7a1fa
V4G4X <varun.gawande@gmail.com>
2023-10-07 23:17:37 +0530

    Added homebrew bundle

--- darwin-configuration.nix
+++ darwin-configuration.nix
@@ -17,6 +17,13 @@
     nodejs
   ];
 
+  homebrew = {
+    enable = true;
+    global.brewfile = true;
+    casks = [ "mqttx" ];
+    onActivation.cleanup = "uninstall";
+  };
+
   # Use a custom configuration.nix location.
   # $ darwin-rebuild switch -I darwin-config=$HOME/.config/nixpkgs/darwin/configuration.nix
   # environment.darwinConfig = "$HOME/.config/nixpkgs/darwin/configuration.nix";
