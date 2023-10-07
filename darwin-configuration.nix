72392fa623c1b220bc072747bbbaaffebab15c8a
V4G4X <varun.gawande@gmail.com>
2023-10-07 20:47:16 +0530

    Further home-manager config

--- darwin-configuration.nix
+++ darwin-configuration.nix
@@ -7,13 +7,14 @@
   # List packages installed in system profile. To search by name, run:
   # $ nix-env -qaP | grep wget
   environment.systemPackages = with pkgs; [
+    wget
     vim
+    rar
     cmake
     ninja
     dfu-util
+    nixpkgs-fmt
     nodejs
-    wget
-    rar
   ];
 
   # Use a custom configuration.nix location.
@@ -34,7 +35,10 @@
 
   # System Configurations
   environment.loginShell = pkgs.zsh;
-  fonts.fonts = [ (pkgs.nerdfonts.override { fonts = ["Meslo" "FiraCode" "FiraMono" ]; }) ];
+  fonts.fonts = [
+    (pkgs.nerdfonts.override { fonts = [ "Meslo" "FiraCode" "FiraMono" ]; })
+  ];
+
   system.defaults.NSGlobalDomain."com.apple.mouse.tapBehavior" = 1;
   system.defaults.NSGlobalDomain."com.apple.swipescrolldirection" = false;
   system.defaults.NSGlobalDomain.KeyRepeat = 1;
@@ -52,24 +56,64 @@
   };
 
   home-manager.users.varungawande = { pkgs, ... }: {
-    home = {
-      packages = with pkgs; [
-        htop
-        fzf
-        direnv
-        gh
-        neovim
-        nixfmt
-        tree
-        zoxide
-        bat
-        lazygit
-        delta
-        ripgrep
-        cargo
-        speedtest-cli
-      ];
-      stateVersion = "22.05";
+
+    programs = {
+      fzf = {
+        enable = true;
+        enableZshIntegration = true;
+      };
+      direnv = {
+        enable = true;
+        enableZshIntegration = true;
+      };
+      gh = {
+        enable = true;
+        settings = {
+          git_protocol = "ssh";
+          prompt = "enabled";
+        };
+      };
+      git = {
+        enable = true;
+        userEmail = "varun.gawande@espressif.com";
+        userName = "Varun Gawande";
+        extraConfig = {
+          core.pager = "delta";
+          interactive.diffFilter = "delta --color-only";
+          delta = {
+            navigate = true;
+            light = false;
+          };
+        };
+      };
+      lazygit = {
+        enable = true;
+        settings = {
+          gui.showFileTree = false;
+          gui.nerdFontsVersion = "3";
+          git.paging.pager = "delta --dark --paging=never";
+          os.editPreset = "nvim";
+        };
+      };
+      neovim = {
+        enable = true;
+        defaultEditor = true;
+        vimAlias = true;
+      };
     };
+
+    home.packages = with pkgs; [
+      bat
+      tree
+      zoxide
+      ripgrep
+      delta
+      cargo
+      htop
+      speedtest-cli
+      yt-dlp
+    ];
+
+    home.stateVersion = "22.05";
   };
 }
---
+++ modules/home-manager/default.nix
@@ -0,0 +1,61 @@
+{ pkgs, ... }: {
+
+  programs = {
+    fzf = {
+      enable = true;
+      enableZshIntegration = true;
+    };
+    direnv = {
+      enable = true;
+      enableZshIntegration = true;
+    };
+    gh = {
+      enable = true;
+      settings = {
+        git_protocol = "ssh";
+        prompt = "enabled";
+      };
+    };
+    git = {
+      enable = true;
+      userEmail = "varun.gawande@espressif.com";
+      userName = "Varun Gawande";
+      extraConfig = {
+        core.pager = "delta";
+        interactive.diffFilter = "delta --color-only";
+        delta = {
+          navigate = true;
+          light = false;
+        };
+      };
+    };
+    lazygit = {
+      enable = true;
+      settings = {
+        gui.showFileTree = false;
+        gui.nerdFontsVersion = "3";
+        git.paging.pager = "delta --dark --paging=never";
+        os.editPreset = "vscode";
+      };
+    };
+    neovim = {
+      enable = true;
+      defaultEditor = true;
+      vimAlias = true;
+    };
+  };
+
+  home.packages = with pkgs; [
+    bat
+    tree
+    zoxide
+    ripgrep
+    delta
+    cargo
+    htop
+    speedtest-cli
+    yt-dlp
+  ];
+
+  home.stateVersion = "22.05";
+}
