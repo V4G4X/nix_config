bdba5b4742557dae88db7d990006bfa68af78a86
V4G4X <varun.gawande@gmail.com>
2023-11-21 16:14:05 +0530

    Brew: Added cfn-lint and nancy

--- modules/homebrew/default.nix
+++ modules/homebrew/default.nix
@@ -1,7 +1,8 @@
 {
   enable = true;
   global.brewfile = true;
-  brews = [ "aws-sam-cli" ];
+  brews = [ "aws-sam-cli" "cfn-lint" "nancy" ];
   casks = [ "mqttx" "docker" ];
+  taps = [ "sonatype-nexus-community/nancy-tap" ];
   onActivation.cleanup = "uninstall";
 }
