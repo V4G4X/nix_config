9beb7f8d7c78537617456e9684b986f1f38c1b55
V4G4X <varun.gawande@gmail.com>
2024-02-26 16:53:30 +0530

    AI: Added ollamac
    A chat client that provides an interface for a local Ollama model

--- modules/homebrew/default.nix
+++ modules/homebrew/default.nix
@@ -2,7 +2,7 @@
   enable = true;
   global.brewfile = true;
   brews = [ "aws-sam-cli" "cfn-lint" "nancy" "graphviz" ];
-  casks = [ "mqttx" "docker" "vlc" ];
+  casks = [ "mqttx" "docker" "vlc" "ollamac" ];
   taps = [ "sonatype-nexus-community/nancy-tap" ];
   onActivation.cleanup = "uninstall";
 }
