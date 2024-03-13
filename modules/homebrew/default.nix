8e0f20e8baaea266ea86314bdaca031d0f7972b5
V4G4X <varun.gawande@gmail.com>
2024-03-13 11:09:53 +0530

    Added Amethyst
    Automatic Window Tiler

--- modules/homebrew/default.nix
+++ modules/homebrew/default.nix
@@ -2,7 +2,7 @@
   enable = true;
   global.brewfile = true;
   brews = [ "aws-sam-cli" "cfn-lint" "nancy" "graphviz" "node" "tabbyml/tabby/tabby" ];
-  casks = [ "mqttx" "docker" "vlc" "ollamac" ];
+  casks = [ "mqttx" "docker" "vlc" "ollamac" "amethyst" ];
   taps = [ "sonatype-nexus-community/nancy-tap" ];
   onActivation.cleanup = "uninstall";
 }
