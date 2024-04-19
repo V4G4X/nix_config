f728dbb0b3b472faf91ab5751732159d8ae49e75
V4G4X <varun.gawande@gmail.com>
2024-04-19 19:49:52 +0530

    Removed Tabby in favour of Codeium

--- modules/homebrew/default.nix
+++ modules/homebrew/default.nix
@@ -1,8 +1,8 @@
 {
   enable = true;
   global.brewfile = true;
-  brews = [ "aws-sam-cli" "cfn-lint" "nancy" "graphviz" "node" "tabbyml/tabby/tabby" ];
   casks = [ "mqttx" "docker" "vlc" "ollamac" "amethyst" ];
+  brews = [ "aws-sam-cli" "cfn-lint" "nancy" "graphviz" "node" ];
   taps = [ "sonatype-nexus-community/nancy-tap" ];
   onActivation.cleanup = "uninstall";
 }
