0e8f1ff837c8c16bdddc224cbf5252dc37716d37
V4G4X <varun.gawande@gmail.com>
2025-02-03 14:53:34 +0530

    [brew] Added Leader-Key
    https://github.com/mikker/LeaderKey.app

--- modules/homebrew/default.nix
+++ modules/homebrew/default.nix
@@ -2,7 +2,7 @@
   enable = true;
   global.brewfile = true;
   brews = [ "aws-sam-cli" "cfn-lint" "nancy" "graphviz" "node" "git-delta" ];
-  casks = [ "mqttx" "docker" "vlc" "ollamac" "amethyst" "iina" "wezterm"];
+  casks = [ "mqttx" "docker" "vlc" "ollamac" "amethyst" "iina" "wezterm" "leader-key" ];
   taps = [ "sonatype-nexus-community/nancy-tap" ];
   onActivation.cleanup = "uninstall";
 }
