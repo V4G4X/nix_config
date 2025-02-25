b20290de641edd5ce2d177c91b49895fb0c0af5a
V4G4X <varun.gawande@gmail.com>
2025-02-25 14:53:03 +0530

    [Macos] Removed problematic dock options
    Nix didn't play nice with these options, would just straight up nuke my
    dock.

--- modules/nix-darwin/darwin-configuration.nix
+++ modules/nix-darwin/darwin-configuration.nix
@@ -48,8 +48,5 @@
   system.defaults.NSGlobalDomain."com.apple.mouse.tapBehavior" = 1;
   system.defaults.NSGlobalDomain."com.apple.swipescrolldirection" = false;
   system.defaults.NSGlobalDomain.KeyRepeat = 1;
-  system.defaults.dock.autohide = true;
-  system.defaults.dock.static-only = true;
-  system.defaults.dock.wvous-bl-corner = 2; # Bottom-Left hot corner is "Mission Control"
   system.defaults.finder.AppleShowAllExtensions = true;
 }
