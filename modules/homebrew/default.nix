b56ccc30e9bd4ddd7bd960e83a35cb60e3e2dc88
V4G4X <varun.gawande@gmail.com>
2025-06-11 11:45:12 +0530

    [Brew] Added OpenCode
    Explicitly added taps because that just cleaned up with fewer warnings.

--- modules/homebrew/default.nix
+++ modules/homebrew/default.nix
@@ -1,8 +1,8 @@
 {
   enable = true;
   global.brewfile = true;
-  brews = [ "aws-sam-cli" "cfn-lint" "nancy" "graphviz" "node" "git-delta" "imagemagick" "yq" ];
-  casks = [ "mqttx" "docker" "vlc" "ollamac" "amethyst" "iina" "wezterm" "leader-key" "nikitabobko/tap/aerospace" ];
-  taps = [ "sonatype-nexus-community/nancy-tap" ];
+  brews = [ "aws-sam-cli" "cfn-lint" "nancy" "graphviz" "node" "git-delta" "imagemagick" "yq" "opencode" ];
+  casks = [ "mqttx" "docker" "vlc" "ollamac" "amethyst" "iina" "wezterm" "leader-key" "aerospace" ];
+  taps = [ "sonatype-nexus-community/nancy-tap" "nikitabobko/tap" "opencode-ai/tap" ];
   onActivation.cleanup = "uninstall";
 }
