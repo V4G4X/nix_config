c8f42f09aa65a8a8dba24af9edcfff7488f0cfde
V4G4X <varun.gawande@gmail.com>
2025-07-24 04:00:24 +0530

    Removed OpenCode

--- modules/homebrew/default.nix
+++ modules/homebrew/default.nix
@@ -1,8 +1,8 @@
 {
   enable = true;
   global.brewfile = true;
-  brews = [ "aws-sam-cli" "cfn-lint" "nancy" "graphviz" "node" "git-delta" "imagemagick" "yq" "opencode" ];
+  brews = [ "aws-sam-cli" "cfn-lint" "nancy" "graphviz" "node" "git-delta" "imagemagick" "yq" ];
   casks = [ "mqttx" "docker" "vlc" "ollamac" "amethyst" "iina" "wezterm" "leader-key" "aerospace" ];
-  taps = [ "sonatype-nexus-community/nancy-tap" "nikitabobko/tap" "opencode-ai/tap" ];
+  taps = [ "sonatype-nexus-community/nancy-tap" "nikitabobko/tap" ];
   onActivation.cleanup = "uninstall";
 }
